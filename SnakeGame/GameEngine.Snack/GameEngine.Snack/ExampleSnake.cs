ff5c6c8e16c9b3c5f451b000a8c3858776ffd259
AizekAzimoff <47439595+AizekAzimoff@users.noreply.github.com>
2019-09-21 20:49:02 +0600

    I changed code in SnakeGame and in SnakeExample.
    I added in SnakeGame function that gives me coordinates of food AND creation of food, I edit randomizer to get food onli inside of board
    And in SnakeExample I added part of code about direction. Now Snake always checks similarity in X and Y axis and if they are same tries to turn in right way to eat food.

---
+++ SnakeGame/GameEngine.Snack/GameEngine.Snack/ExampleSnake.cs
@@ -0,0 +1,104 @@
+﻿using PixelEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Snack
+{
+    public class ExampleSnake : ISnake
+    {
+        private const int _width = 50;
+        private const int _wallDistanceThreshold = 5;
+        private Point _myHeadPosition;
+
+        public string Name => "Aziz";
+
+        public SnakeDirection GetNextDirection(SnakeDirection currentDirection)
+        {
+
+            if(currentDirection == SnakeDirection.Up 
+                && _myHeadPosition.Y < _wallDistanceThreshold)
+            {
+                return SnakeDirection.Left;
+            }
+
+            if(currentDirection == SnakeDirection.Right
+                && _myHeadPosition.X > _width - _wallDistanceThreshold)
+            {
+                return SnakeDirection.Up;
+            }
+
+            if(currentDirection == SnakeDirection.Down
+                && _myHeadPosition.Y > _width - _wallDistanceThreshold)
+            {
+                return SnakeDirection.Right;
+            }
+
+            if (currentDirection == SnakeDirection.Left
+                && _myHeadPosition.X <  _wallDistanceThreshold)
+            {
+                return SnakeDirection.Down;
+            }
+
+            Tuple<int, int> foodCoord = SnakeGame.getFoodCoord();
+            int foodX = foodCoord.Item1;
+            int foodY = foodCoord.Item2;
+
+            if (currentDirection == SnakeDirection.Up
+                && _myHeadPosition.Y == foodY && _myHeadPosition.X < foodX)
+            {
+                return SnakeDirection.Right;
+            }
+            else if (currentDirection == SnakeDirection.Up
+   && _myHeadPosition.Y == foodY && _myHeadPosition.X > foodX)
+            {
+                return SnakeDirection.Left;
+            }
+
+            else if (currentDirection == SnakeDirection.Right
+                 && _myHeadPosition.X == foodX && _myHeadPosition.Y > foodY)
+            {
+                return SnakeDirection.Up;
+            }
+            else if (currentDirection == SnakeDirection.Right
+    && _myHeadPosition.X == foodX && _myHeadPosition.Y < foodY)
+            {
+                return SnakeDirection.Down;
+            }
+            else if (currentDirection == SnakeDirection.Left
+                 && _myHeadPosition.X == foodX && _myHeadPosition.Y < foodY)
+            {
+                return SnakeDirection.Down;
+            }
+            else if (currentDirection == SnakeDirection.Left
+                 && _myHeadPosition.X == foodX && _myHeadPosition.Y > foodY)
+            {
+                return SnakeDirection.Up;
+            }
+            else if (currentDirection == SnakeDirection.Down
+                && _myHeadPosition.Y == foodY && _myHeadPosition.X < foodX)
+            {
+                return SnakeDirection.Right;
+            }
+            else if (currentDirection == SnakeDirection.Down
+   && _myHeadPosition.Y == foodY && _myHeadPosition.X > foodX)
+            {
+                return SnakeDirection.Left;
+            }
+            return currentDirection;
+        }
+
+        public void UpdateMap(string map)
+        {
+            _myHeadPosition = getMyHeadPosition(map);
+        }
+
+        private Point getMyHeadPosition(string map)
+        {
+            var headIndex = map.IndexOf('*');
+            return new Point(headIndex % _width, headIndex / _width);
+        }
+    }
+}
