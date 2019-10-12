bd1ec1fa5e43df8f0e489c5215596c7ac7e06be0
AizekAzimoff <47439595+AizekAzimoff@users.noreply.github.com>
2019-10-12 23:11:07 +0600

    I made this tutorial as far as possible
    Because at the middle of it appeared some error that says about incompatibility of my version of program and the code in that i write in core package

---
+++ exercise5/exercise/Data/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using exercise.Models.Movies;
+
+namespace exercise.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
